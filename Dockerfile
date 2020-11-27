fcf43ef30b2baeb06f44bee8bb0b06bb9d6b80e8
Dietrich Zinsou <dietrich.zinsou@hpe.com>
2020-11-27 13:36:58 +0100

    Update centos kd app

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM centos:7.8.2003
 LABEL maintainer="dietrich.zinsou@hpe.com"
 
-RUN yum install -y wget curl tar unzip python python-setuptools python-argparse python-sssdconfig python-ldap
+RUN yum install -y curl tar python python-setuptools python-argparse
+
+COPY appconfig.tgz /opt/configscripts/appconfig.tgz
 
 CMD ["/sbin/init"]
--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Clean and create appconfig archive
+rm -rf ./kubedirector/appconfig.tgz
+cd ./kubedirector && tar czf appconfig.tgz appconfig/ && cd -
+mv ./kubedirector/appconfig.tgz .
+
+# Build and push Docker image
 docker build -t dzinsouhpe/centos:7.8.2003 .
 docker push dzinsouhpe/centos:7.8.2003
 
+docker build -t dzinsouhpe/centos:latest .
+docker push dzinsouhpe/centos:latest
+
+# Delete temporary archive
+rm -rf ./appconfig.tgz
